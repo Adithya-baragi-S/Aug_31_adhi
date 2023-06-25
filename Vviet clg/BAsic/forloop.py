feef172823a973dde89d280a4321b101ebeac91f
adith <adith@Aditya>
2023-06-25 16:09:52 +0530

    kar

---
+++ Vviet clg/BAsic/forloop.py
@@ -0,0 +1,40 @@
+'''
+
+for x in range(1,6,2):
+    print(x ,end=" ")
+'''
+from _tracemalloc import stop
+
+'''
+*
+**
+***
+****
+*****
+******
+'''
+'''
+for x in range(0,7):
+    print("*" * x)
+'''  
+  
+'''
+for x in range(7,0,-1):
+    print("*" * x) 
+    '''
+    
+'''
+starting POINT
+maximum POINT
+end POINT
+'''
+
+"start point will accept both pos and neg at end point is only pos"
+"in another case we can take both neg value start point should be greater than end point"
+'''
+for x in range(10,0,-2):
+    print(x)
+when start is > stop 
+when start is < stop
+'''
+
