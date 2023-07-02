dc082b67b509c8095a1b8fe6aaafed8610f99871
adith <adith@Adithya>
2023-07-02 08:30:06 +0530

    today

---
+++ Vviet clg/BAsic/assignment01.py
@@ -0,0 +1,34 @@
+
+'''
+for x in range(1,6):
+    print("*" * x)
+    
+for y in range(6,0,-1):
+    print("*" * y)
+    ''' 
+    
+'''
+for i in range(5):
+    for j in range(i+1):
+        print(chr(j + 65),end=" ")
+        '''
+'''   
+for i in range (6):
+    for j in range (i+1):
+                print(j,end=" ")     
+                '''
+                
+'''
+num =int(input("enter a value for num"))
+factorial=1
+if num < 0:
+    print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+    print("The factorial of 0 is 1")
+else:
+    for i in range(1,num+1):
+        factorial = factorial*i
+        print("The factorial of is",factorial)
+        '''
+        
+    
